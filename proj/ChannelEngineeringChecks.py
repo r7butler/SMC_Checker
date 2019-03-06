e0ce27ee72fb731ab0e993e111504a0bc69d9c46
Paul Smith <pauls@sccwrp.org>
2019-03-06 14:58:44 -0800

    Added changes to ChannelEngineeringChecks.py March 6 2019

--- proj/ChannelEngineeringChecks.py
+++ proj/ChannelEngineeringChecks.py
@@ -1,11 +1,20 @@
+#############
+## IMPORTS ##
+#############
 from flask import Blueprint, request, jsonify, session
+import os, time, datetime
 import pandas as pd
 import numpy as np
 import json
 from sqlalchemy import create_engine, exc, Table, Column, Integer, Unicode, MetaData, String, Text, update, and_, select, func, types
 from pandas import DataFrame
+from datetime import datetime
 from .ApplicationLog import *
 
+
+#####################
+## ERROR FUNCTIONS ##
+#####################
 def addErrorToList(error_column, row, error_to_add,df):
 	df.ix[int(row), 'row'] = str(row)
 	if error_column in df.columns:
@@ -37,25 +46,35 @@ def getCustomErrors(df,name,warn_or_error):
 	tab_value = name.strip().split(" - ")
 	tab = tab_value[0]
 	# get tab number of dataframe batch = 0, result = 1, wq = 2
+	#errorLog(tab[0])
+	#errorLog(list(df))
 	#  clear dataframe of rows that have no errors
 	dfjson = df
         dfjson = dfjson[pd.notnull(dfjson[warn_or_error])]
+	#errorLog(dfjson)
+        #dfjson = dfjson[pd.notnull(dfjson['toxicity_errors'])]
         # must re-index dataframe - set to 0 after removing rows
 	# not necessary with custom errors only regular
+	#dfjson.reset_index(drop=True,inplace=True) 
 	tmp_dict = {}
 	count = 0
 	# Critical for custom checks we look at row instead of tmp_row
 	# something must be wrong with code row seems to work with toxicity and summary checks, but tmp_row fails to work properly with summary (duplicates)
 	for index, row in dfjson.iterrows():
+		# delete - errorLog("count: %s - row: %s value: %s" % (count,row['tmp_row'],row['custom_errors']))
+		# delete - tmp_dict[tab] = '[{"count":"%s","row":"%s","value":[%s]}]' % (count,row['tmp_row'],row['custom_errors'])
 		tabcount = tab + "-" + str(count)
 		tmp_dict[tabcount] = '[{"row":"%s","value":[%s]}]' % (row['row'],row[warn_or_error])
 		errorLog("row: %s, value: %s" % (row['row'],row[warn_or_error]))
+		# delete - tmp_dict[tabcount] = '[{"row":"%s","value":[%s]}]' % (row['tmp_row'],row['custom_errors'])
 		count = count + 1
 	errorLog("end getCustomErrors")
 	return tmp_dict
 
+
 def getCustomRedundantErrors(df,name,check):
 	errorLog("start getCustomRedundantErrors")
+	#errorLog("check: %s" % check)
 	# get name of dataframe
 	tab_value = name.strip().split(" - ")
 	# get tab number of dataframe batch = 0, result = 1, wq = 2
@@ -96,9 +115,16 @@ def dcValueAgainstMultipleValues(eng,dbtable,dbfield,df,field):
         subcodes['check'] = subcodes[field].dropna().apply(lambda row: set("".join(row.split()).lower().split(',')).issubset(db_list))
 	invalid_codes = df.loc[subcodes.check == False].tmp_row.tolist()
 	return nan_rows, invalid_codes, subcodes
+        
+                                            
+
+
+
+######################################
+## BEGIN CHANNEL ENGINEERING SCRIPT ##
+######################################
 
 channelengineering_checks = Blueprint('channelengineering_checks', __name__)
-
 @channelengineering_checks.route("/channelengineering", methods=["POST"])
 
 def channelengineering(all_dataframes,sql_match_tables,errors_dict,project_code,login_info):
@@ -119,24 +145,34 @@ def channelengineering(all_dataframes,sql_match_tables,errors_dict,project_code,
         summary_checks = ""
         summary_results_link = ""
         custom_redundant_checks = ""
+                                            
+                                            
         custom_errors = []
         custom_warnings = []
         custom_redundant_errors = []
         custom_redundant_warnings = []
-
 	TIMESTAMP=str(session.get('key'))
+        ### get date and time
+        gettime = int(time.time())
+        timestamp_date = datetime.datetime.fromtimestamp(gettime)
 	# add submitted table names to list
 	tables = []
 	# match tablenames to tabs
 	errorLog(all_dataframes.keys())
 	for dataframe in all_dataframes.keys():
 		df_sheet_and_table_name = dataframe.strip().split(" - ")
+		errorLog(df_sheet_and_table_name)
 		table_name = str(df_sheet_and_table_name[2])
+		errorLog(table_name)
 		if table_name == "tbl_channelengineering":
-			channel = all_dataframes[dataframe]
-			channel['tmp_row'] = channel.index
+			tables.append("channelengineering")
+			chaneng = all_dataframes[dataframe]
+			chaneng['tmp_row'] = chaneng.index
+
 	try:
-		## CHECKS ##
+                #####################
+		## CHECK FUNCTIONS ##
+                #####################
 		def checkData(statement,column,warn_or_error,error_label,human_error,dataframe):
 			errorLog("checkData warn_or_error: %s" % error_label)
 			for item_number in statement:
@@ -153,6 +189,144 @@ def channelengineering(all_dataframes,sql_match_tables,errors_dict,project_code,
 				addErrorToList("custom_errors",item_number,unique_error,dataframe)
 				errorsCount(errors_dict,'custom')
 
+
+                # Robert - If "Other" for the following fields then corresponding comment field required (ie. If bottom = "Other" then bottomcomments requires a value):
+                
+                # - bottom
+                # - determination
+                # - leftsideofstructure
+                # - rightsideofstructure
+                # - structureshape
+
+                # - bottom - # 
+                errorLog('Check - If Other for the bottom field then corresponding bottomcomments field is required')
+                errorLog('Submitted Data where Bottom field is Other, but no comment:')
+                errorLog(chaneng[(chaneng.bottom == 'Other')&(chaneng.bottomcomments.isnull())])
+                checkData(chaneng[(chaneng.bottom == 'Other')&(chaneng.bottomcomments.isnull())].tmp_row.tolist(),'bottomcomments','Undefined Error','error','You have entered Other for bottom field, comment is required.',chaneng)
+
+                # - determination - # 
+                errorLog('Check - If Other for the determination field then corresponding determinationcomments field is required')
+                errorLog('Submitted Data where determination field is Other, but no comment:')
+                errorLog(chaneng[(chaneng.determination == 'Other')&(chaneng.determinationcomments.isnull())])
+                checkData(chaneng[(chaneng.determination == 'Other')&(chaneng.determinationcomments.isnull())].tmp_row.tolist(),'determinationcomments','Undefined Error','error','You have entered Other for determination field, comment is required.',chaneng)
+
+                # - leftsideofstructure - # 
+                errorLog('Check - If Other for the leftsideofstructure field then corresponding leftsidecomments field is required')
+                errorLog('Submitted Data where leftsideofstructure field is Other, but no comment:')
+                errorLog(chaneng[(chaneng.leftsideofstructure == 'Other')&(chaneng.leftsidecomments.isnull())])
+                checkData(chaneng[(chaneng.leftsideofstructure == 'Other')&(chaneng.leftsidecomments.isnull())].tmp_row.tolist(),'leftsidecomments','Undefined Error','error','You have entered Other for leftsideofstructure field, comment is required.',chaneng)
+
+                # - rightsideofstructure - # 
+                errorLog('Check - If Other for the rightsideofstructure field then corresponding rightsidecomments field is required')
+                errorLog('Submitted Data where rightsideofstructure field is Other, but no comment:')
+                errorLog(chaneng[(chaneng.rightsideofstructure == 'Other')&(chaneng.rightsidecomments.isnull())])
+                checkData(chaneng[(chaneng.rightsideofstructure == 'Other')&(chaneng.rightsidecomments.isnull())].tmp_row.tolist(),'rightsidecomments','Undefined Error','error','You have entered Other for rightsideofstructure field, comment is required.',chaneng)
+
+                # - structureshape - # 
+                errorLog('Check - If Other for the structureshape field then corresponding structureshapecomments field is required')
+                errorLog('Submitted Data where structureshape field is Other, but no comment:')
+                errorLog(chaneng[(chaneng.structureshape == 'Other')&(chaneng.structureshapecomments.isnull())])
+                checkData(chaneng[(chaneng.structureshape == 'Other')&(chaneng.structureshapecomments.isnull())].tmp_row.tolist(),'structureshapecomments','Undefined Error','error','You have entered Other for structureshape field, comment is required.',chaneng)
+
+
+
+
+
+                
+                # Robert - 12/6/2018
+                # This check appears to be taken care of in CoreChecks. Need confirmation.
+                '''
+                # Robert 12/6/2018 #
+                # Check - if channeltype == "Engineered" then the following fields are required:
+                # - bottom 
+                # - structurewidth
+                # - structureshape
+                # - leftsideofstructure
+                # - leftvegetation
+                                            
+                                            
+                # - rightsideofstructure
+                # - rightvegetation
+                # - vegetation
+                # - lowflowpresence
+                # - lowflowwidth
+                def EngineeredChannelChecks(fieldname):
+                    errorLog('Check - if channeltype == Engineered then the %s field is required' % fieldname)
+                    errorLog('Submitted Data where channeltype == Engineered but the %s is missing:' % fieldname)
+                    errorLog(chaneng[ (chaneng.channeltype == 'Engineered') & (chaneng[fieldname].isnull()) ])
+                    checkData(chaneng[ (chaneng.channeltype == 'Engineered') & (chaneng[fieldname].isnull()) ].tmp_row.tolist(), fieldname, 'Undefined Error', 'error', 'The channeltype is Engineered, but the %s field is missing' % fieldname, chaneng)
+
+                EngineeredChannelChecks('bottom')
+                EngineeredChannelChecks('structurewidth')
+                EngineeredChannelChecks('structureshape')
+                EngineeredChannelChecks('leftsideofstructure')
+                EngineeredChannelChecks('leftvegetation')
+                EngineeredChannelChecks('rightsideofstructure')
+                EngineeredChannelChecks('rightvegetation')
+                EngineeredChannelChecks('vegetation')
+                EngineeredChannelChecks('lowflowpresence')
+                EngineeredChannelChecks('lowflowwidth')
+                '''
+
+
+
+                # Robert 12/6/2018 #
+                # Check - if channeltype == Natural then the following fields should be filled with default empty values (-88 and NR)
+                # - bottom 
+                # - structurewidth
+                # - structureshape
+                # - leftsideofstructure
+                # - leftvegetation
+                # - rightsideofstructure
+                # - rightvegetation
+                # - vegetation
+                # - lowflowpresence
+                # - lowflowwidth
+                def NaturalChannelCheck(fieldname):
+                    errorLog('Check - if channeltype == Natural then the %s field should be filled out as NR' % fieldname)
+                    errorLog('Submitted Data where channeltype == Natural but the %s field is not filled with NR:' % fieldname)
+                    errorLog(chaneng[ (chaneng.channeltype == 'Natural') & (chaneng[fieldname] != 'NR')  ])
+                    checkData(chaneng[ (chaneng.channeltype == 'Natural') & (chaneng[fieldname] != 'NR') ].tmp_row.tolist(), fieldname, 'Undefined Error', 'error', 'The channeltype is Natural, but the %s field is not filled with NR' % fieldname, chaneng)
+
+                NaturalChannelCheck('bottom')    
+                NaturalChannelCheck('structurewidth')    
+                NaturalChannelCheck('structureshape')    
+                NaturalChannelCheck('leftsideofstructure')    
+                NaturalChannelCheck('leftvegetation')    
+                NaturalChannelCheck('rightsideofstructure')    
+                NaturalChannelCheck('rightvegetation')    
+                NaturalChannelCheck('vegetation')    
+                NaturalChannelCheck('lowflowpresence')    
+                NaturalChannelCheck('lowflowwidth')    
+                
+
+
+                # Robert - 12/6/2018
+                # This check appears to be taken care of in CoreChecks. Need Confirmation.
+                '''
+                # Robert 12/6/2018
+                # Check - if lowflowpresence == "Present" then lowflowwidth is required
+                errorLog('Check - if lowflowpresence == Present then lowflowwidth is required')
+                errorLog('Submitted Data where lowflowpresence == Present but the lowflowwidth field is missing:')
+                errorLog(chaneng[ (chaneng.lowflowpresence == 'Present') & (chaneng.lowflowwidth.isnull()) ])
+                checkData(chaneng[ (chaneng.lowflowpresence == 'Present') & (chaneng.lowflowwidth.isnull()) ].tmp_row.tolist(), 'lowflowwidth', 'Undefined Error', 'error', 'The lowflowpresence field is recorded as Present, but the lowflowwidth field is missing', chaneng)
+                '''
+
+                # Robert - 12/6/2018
+                # This check appears to be taken care of in CoreChecks. Need Confirmation.
+                '''
+                # Robert 12/6/2018
+                # Check - if gradecontrolpresence  == "Present" then gradecontrollocation is required
+                errorLog('Check - if gradecontrolpresence == Present then gradecontrollocation is required')
+                errorLog('Submitted Data where gradecontrolpresence == Present but the gradecontrollocation field is missing:')
+                errorLog(chaneng[ (chaneng.gradecontrolpresence == 'Present') & (chaneng.gradecontrollocation.isnull()) ])
+                checkData(chaneng[ (chaneng.lowflowpresence == 'Present') & (chaneng.gradecontrollocation.isnull()) ].tmp_row.tolist(), 'gradecontrollocation', 'Undefined Error', 'error', 'The gradecontrolpresence field is recorded as Present, but the gradecontrollocation field is missing', chaneng)
+                '''
+               
+
+
+
+
 		## LOGIC ##
 		message = "Starting ChannelEngineering Logic Checks"
 		errorLog(message)
@@ -163,7 +337,7 @@ def channelengineering(all_dataframes,sql_match_tables,errors_dict,project_code,
 		## END CUSTOM CHECKS ##
 		## START MAP CHECK ##
 		# get a unique list of stations from stationcode
-		list_of_stations = pd.unique(channel['stationcode'])
+		list_of_stations = pd.unique(chaneng['stationcode'])
 		unique_stations = ','.join("'" + s + "'" for s in list_of_stations)
 		## END MAP CHECKS
 		## END CHECKS ##
@@ -185,13 +359,19 @@ def channelengineering(all_dataframes,sql_match_tables,errors_dict,project_code,
 		custom_redundant_checks = json.dumps(custom_redundant_errors, ensure_ascii=True)
 		## END RETRIEVE ERRORS ##
 		# get filenames from fileupload routine
-		errorLog(message)
-		#assignment_table = result.groupby(['stationid','lab','analyteclass']).size().to_frame(name = 'count').reset_index()
+		
+                #assignment_table = result.groupby(['stationid','lab','analyteclass']).size().to_frame(name = 'count').reset_index()
 		# lets reassign the analyteclass field name to species so the assignment query will run properly - check StagingUpload.py for details
 		#assignment_table = assignment_table.rename(columns={'analyteclass': 'species'})
-		return custom_checks, custom_redundant_checks, message, unique_stations
-	except ValueError:
+		
+                message = "Channel Engineering Checks completed successfully"
+                errorLog(message)
+                statusLog(message)
+
+                return custom_checks, custom_redundant_checks, message, unique_stations
+	except Exception as e:
 		message = "Critical Error: Failed to run channelengineering checks"	
 		errorLog(message)
+                errorLog(e)
 		state = 1
 		return jsonify(message=message,state=state)
